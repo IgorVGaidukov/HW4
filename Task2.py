d409bbcb115a22609917c7813e9845a17cf09258
Admin <msk_moz_info@kinomax.ru>
2023-01-31 13:05:20 +0300

    Add solution

--- Task2.py
+++ Task2.py
@@ -8,3 +8,18 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+
+def user_info(name, last_name, year, town, email, tel):
+    print(f'{name} {last_name} {year} года рождения, проживает в городе {town}, email: {email}, телефон: {tel}')
+
+
+user_name = input('Введите имя: ')
+user_last_name = input('Введите фамилию: ')
+user_year = int(input('Введите год рождения: '))
+user_town = input('Введите город проживания: ')
+user_email = input('Введите email: ')
+user_tel = input('Введите телефон: ')
+
+user_info(name=user_name, last_name=user_last_name, year=user_year,
+          town=user_town, email=user_email, tel=user_tel)
--- Task5.py
+++ Task5.py
@@ -2,3 +2,31 @@
 5. Сделайте профилирование кода любого или любых выполненных заданий
 с помощью модуля timeit, опишите результат
 """
+
+from timeit import timeit
+
+
+def my_func1(num1, num2, num3):
+    lst = []
+    lst.extend([num1, num2, num3])
+    lst.sort()
+    return lst[-1] + lst[-2]
+
+
+def my_func2(num1, num2, num3):
+    lst = []
+    lst.extend([num1, num2, num3])
+    max1 = max(lst)
+    lst.remove(max1)
+    return max1 + max(lst)
+
+
+n1 = int(input('Введите число 1: '))
+n2 = int(input('Введите число 2: '))
+n3 = int(input('Введите число 3: '))
+
+print('Производительность функций при повторении 10000 раз')
+print('my_func1 (с функцией sort):', timeit(
+    "my_func1(n1, n2, n3)", globals=globals(), number=10000))
+print('my_func2 (без функциеи sort):', timeit(
+    "my_func2(n1, n2, n3)", globals=globals(), number=10000))
